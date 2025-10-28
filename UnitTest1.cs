8ee60da52c690ef035ea680abf2dd81931a38933
wendy.depass <wendy.depass@AUTOP-JGPOBBGGC>
2025-10-27 20:13:27 -0600

    Agregar archivos de proyecto.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Clase_1_Automatizacion_27_10_2025
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
