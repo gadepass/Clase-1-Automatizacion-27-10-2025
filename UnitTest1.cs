d11853c1c45dca90126068e52d568c08bbc98be2
gadepass <gabydpmiranda@gmail.com>
2025-10-27 20:42:09 -0600

    Se realiza la primera versión 27-10-2025

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,16 +1,30 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
 namespace Clase_1_Automatizacion_27_10_2025
 {
     public class Tests
     {
+        private IWebDriver driver; //Variable privada que se inicializa en el setup
+
         [SetUp]
         public void Setup()
         {
+            var options = new EdgeOptions(); //Se crea la variable, hace el llamado al driver que se utilizará
+            driver = new EdgeDriver(options);
         }
 
-        [Test]
-        public void Test1()
+        [Test] //Los Test o el etiquetado de test siempre deben estar.
+        public void TestNavegar()
         {
-            Assert.Pass();
+            driver.Navigate().GoToUrl("https://www.bing.com"); //Siempre lleva punto y coma                                                  
+            driver.Manage().Window.Maximize(); //para maximizar la ventana del navegador.
+            // Assert.Pass();
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit(); //Quito o cierro el driver.
         }
     }
 }
